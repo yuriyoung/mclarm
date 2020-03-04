cef5a8e17d23c850edbe1368da6bb1db271745d5
Yuri Young <yuri.young@qq.com>
2020-03-04 15:40:00 +0800

    add socialite oauth routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Social Auth
+Route::group(['namespace' => 'Auth'], function () {
+    Route::get('oauth/{provider}', 'LoginController@oauthRedirect')->name('oauth.redirect');
+    Route::get('oauth/{provider}/callback', 'LoginController@oauthCallback')->name('oauth.callback');
 });
