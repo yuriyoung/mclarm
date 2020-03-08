d3ec80a7babe3a48be7576da766450f51af2c8b3
Yuri Young <yuri.young@qq.com>
2020-03-08 15:44:28 +0800

    add jwt auth routes

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Api'], function () {
+    Route::post('login', 'AuthController@login')->name('api.auth.login');
+    Route::post('register', 'AuthController@register')->name('api.auth.register');
+    Route::middleware('jwt.auth')->post('logout', 'AuthController@logout')->name('api.auth.logout');;
+    Route::middleware('jwt.auth')->get('me', 'AuthController@me')->name('api.auth.me');
+    Route::middleware('jwt.refresh')->post('refresh', 'AuthController@refresh')->name('api.auth.refresh');
 });
 
-Route::group(['prefix' => 'v1', 'namespace' => 'Api'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => 'jwt.auth', 'namespace' => 'Api'], function () {
     Route::apiResource('users', 'UserController');
     Route::get('users/{user}/detail', 'UserController@detail')->name('users.detail');
 });
+
+/**
+ * protected $middlewareAliases = [
+    'jwt.auth' => Authenticate::class,
+    'jwt.check' => Check::class,
+    'jwt.refresh' => RefreshToken::class, // response a token at the header
+    'jwt.renew' => AuthenticateAndRenew::class,
+];
+ */
