49150f253bf53f19133daabeac556ba7915ea333
Yuri Young <yuri.young@qq.com>
2020-03-08 15:37:25 +0800

    add social account table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,30 +15,52 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique(); // Represents a username
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();;
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
 
+        // accounts
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('email')->nullable();
+            $table->string('provider_name');
+            $table->string('provider_id')->nullable();
+            $table->string('access_token')->nullable();;
+            $table->string('refresh_token')->nullable();
+            $table->timestamp('expires_in')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
         Schema::create('user_details', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
-            $table->string('avatar')->nullable()->comment('头像');
-            $table->string('qrcode')->nullable()->comment('二维码');
-            $table->string('first_name')->nullable()->comment('名');
-            $table->string('last_name')->nullable()->comment('姓');
-            $table->enum('gender', ['male', 'female', 'neuter'])->comment('性别'); // 0-male, 1-female, 2-neuter
-            $table->string('birthday')->nullable()->comment('生日');
-            $table->string('career')->nullable()->comment('职业');
-            $table->string('website')->nullable()->comment('主页');
-            $table->string('github')->nullable()->comment('GitHub');
+            $table->string('nickname')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('qrcode')->nullable();
+            $table->string('gravatar')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('location')->nullable();
+            $table->string('company')->nullable();
+            $table->enum('gender', ['male', 'female', 'neuter']); // 0-male, 1-female, 2-neuter
+            $table->string('birthday')->nullable();
+            $table->string('career')->nullable();
+            $table->string('website')->nullable();
+            $table->string('github')->nullable();
             $table->string('address_home')->nullable();
             $table->string('address_work')->nullable();
-            $table->string('signature')->nullable()->comment('签名');;
-            $table->longText('about')->nullable()->comment('关于我');;
+            $table->string('bio')->nullable();;
+            $table->longText('about')->nullable();;
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
@@ -47,7 +69,7 @@ class CreateUsersTable extends Migration
         Schema::create('banned_users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
-            $table->longText('reason')->comment('原因');
+            $table->longText('reason');
             $table->unsignedInteger('days')->default(7);
             $table->timestamp('banned_at')->useCurrent();
 
@@ -57,11 +79,11 @@ class CreateUsersTable extends Migration
         Schema::create('user_signed_logs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
-            $table->string('device')->nullable()->comment('登录设备');
-            $table->string('platform')->nullable()->comment('登录平台');
-            $table->string('client')->nullable()->comment('登录客户端');
-            $table->string('ip')->nullable()->comment('登录ip');
-            $table->timestamp('signed_at')->useCurrent()->comment('登录日期');
+            $table->string('device')->nullable();
+            $table->string('platform')->nullable();
+            $table->string('client')->nullable();
+            $table->string('ip')->nullable();
+            $table->timestamp('signed_at')->useCurrent();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
@@ -77,6 +99,7 @@ class CreateUsersTable extends Migration
         Schema::dropIfExists('user_signed_logs');
         Schema::dropIfExists('banned_users');
         Schema::dropIfExists('user_details');
+        Schema::dropIfExists('social_accounts');
         Schema::dropIfExists('users');
     }
 }
